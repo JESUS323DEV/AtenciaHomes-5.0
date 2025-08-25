4671eaf906309c5a3d4eb2db3382824c616d7f6b
Jesús-DEV <jtipian90@gmail.com>
2025-08-25 17:57:51 +0200

    Add files via upload

--- js/scripts-menu-responsive.js
+++ js/scripts-menu-responsive.js
@@ -1,29 +1,45 @@
 // Cierra el menú hamburguesa al hacer clic en un enlace interno
 document.querySelectorAll('.navbar-nav a[href^="#"]').forEach(link => {
-    link.addEventListener('click', function () {
-        const navbarCollapse = document.getElementById('navbarSupportedContent');
-        const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
-        if (bsCollapse) {
-            bsCollapse.hide();
-        }
-    });
+  link.addEventListener('click', function () {
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
+    const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+    if (bsCollapse) {
+      bsCollapse.hide();
+    }
+  });
 });
 
 
-// === Cambia la clase del navbar al hacer scroll (para fondo y sombra) ===
+// === Cambia la clase del navbar al hacer scroll (para fondo y sombra) === // 
 const navbar = document.getElementById('navbar');
 window.addEventListener('scroll', () => {
-    navbar.classList.toggle('scrolled', window.scrollY > 50);
+  navbar.classList.toggle('scrolled', window.scrollY > 50);
 });
 // =============================== // 
-  document.querySelectorAll("details").forEach(detail => {
-    const icon = detail.querySelector("summary i");
+document.querySelectorAll("details").forEach(detail => {
+  const icon = detail.querySelector("summary i");
 
-    detail.addEventListener("toggle", () => {
-      if (detail.open) {
-        icon.classList.replace("bi-plus-lg", "bi-x-lg");
-      } else {
-        icon.classList.replace("bi-x-lg", "bi-plus-lg");
-      }
-    });
+  detail.addEventListener("toggle", () => {
+    if (detail.open) {
+      icon.classList.replace("bi-plus-lg", "bi-x-lg");
+    } else {
+      icon.classList.replace("bi-x-lg", "bi-plus-lg");
+    }
   });
+});
+
+
+// ===========QUITA EL BOTÓN DE "ABRIR-GALERÍA"==================== // 
+
+const openBtn = document.querySelector('[data-bs-target="#fullscreenModal"]');
+const galleryModal = document.getElementById('fullscreenModal');
+
+// Cuando se abre el modal
+galleryModal.addEventListener('show.bs.modal', () => {
+  openBtn.style.display = 'none';
+});
+
+// Cuando se cierra el modal
+galleryModal.addEventListener('hidden.bs.modal', () => {
+  openBtn.style.display = 'block';
+});
